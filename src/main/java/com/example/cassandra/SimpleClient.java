4c199dab5ec22446de802107ef28ce5a3aa2f19d
afrete nassim <nass_5902@hotmail.fr>
2014-06-02 11:20:07 +0200

    debug code

--- src/main/java/com/example/cassandra/SimpleClient.java
+++ src/main/java/com/example/cassandra/SimpleClient.java
@@ -1,22 +1,17 @@
 package com.example.cassandra;
 
-import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
 
 public class SimpleClient {
 
     private Cluster cluster;
     private Session session;
-    private boolean keyspaceCreated;
+    private static boolean keyspaceCreated = false;
 
     public void connect(String node) {
         cluster = Cluster.builder().addContactPoint(node).build();
@@ -33,26 +28,34 @@ public class SimpleClient {
     }
 
     public void createSchema() {
-        // création d'un keyspace nommé "simplex"
+        // création d'un keyspace nommé "bddCassandra"
         session.execute("CREATE KEYSPACE bddCassandra WITH replication "
-                + "= {'class':'NetworkTopologyStrategy', 'replication_factor':2};");
-        // creation de la table songs (tags est un tableau)
+                + "= {'class':'SimpleStrategy', 'replication_factor':3};");
         // A changer
         session.execute(
-                "CREATE TABLE bddCassandra.essone ("
-                + "id uuid PRIMARY KEY,"
-                + "title text,"
-                + "album text,"
-                + "artist text,"
-                + "tags set<text>,"
-                + "data blob"
-                + ");");
+                "CREATE TABLE bddCassandra.essone (code_insee int PRIMARY KEY, code_postal int, commune text, wgs84 text, federation text,licences_en_2011 int, moins_de_20_ans int, entre_20_et_60_ans int, plus_de_60_ans int, femmes int, "
+                + "femmes_moins_de_20_ans int, femmes_de_20_a_60_ans int, femmes_plus_de_60_ans int, licences_en_zone_urbaine_sensible_zus int, population_totale_2010 int, population_femme int, population_femmes_de_moins_de_20_ans int, "
+                + "population_femme_de_20_a_60_ans int, population_femme_de_plus_de_60_ans int, population_de_moins_de_20_ans int, population_de_20_a_60_ans int, population_de_plus_de_60_ans int);"
+        );
+
+        session.execute(
+                "CREATE TABLE bddCassandra.seinesaintdenis (code_insee int PRIMARY KEY, code_postal int, commune text, wgs84 text, federation text,licences_en_2011 int, moins_de_20_ans int, entre_20_et_60_ans int, plus_de_60_ans int, femmes int, "
+                + "femmes_moins_de_20_ans int, femmes_de_20_a_60_ans int, femmes_plus_de_60_ans int, licences_en_zone_urbaine_sensible_zus int, population_totale_2010 int, population_femme int, population_femmes_de_moins_de_20_ans int, "
+                + "population_femme_de_20_a_60_ans int, population_femme_de_plus_de_60_ans int, population_de_moins_de_20_ans int, population_de_20_a_60_ans int, population_de_plus_de_60_ans int);"
+        );
+
+        session.execute(
+                "CREATE TABLE bddCassandra.valdemarne (code_insee int PRIMARY KEY, code_postal int, commune text, wgs84 text, federation text,licences_en_2011 int, moins_de_20_ans int, entre_20_et_60_ans int, plus_de_60_ans int, femmes int, "
+                + "femmes_moins_de_20_ans int, femmes_de_20_a_60_ans int, femmes_plus_de_60_ans int, licences_en_zone_urbaine_sensible_zus int, population_totale_2010 int, population_femme int, population_femmes_de_moins_de_20_ans int, "
+                + "population_femme_de_20_a_60_ans int, population_femme_de_plus_de_60_ans int, population_de_moins_de_20_ans int, population_de_20_a_60_ans int, population_de_plus_de_60_ans int);"
+        );
+
         keyspaceCreated = true;
     }
 
     public void queryEssone() {
         // on execute une requête
-        ResultSet results = session.execute("SELECT * FROM sport.essone ");
+        ResultSet results = session.execute("SELECT * FROM bddCassandra.essone ");
         // on affiche le résultat
         System.out.println(String.format("%-10s\t%-10s\t%-20s\t%-70s\t%-30s\t%-30s\t%-30s\t%s-30s", "code_insee", "code_postal", "commune", "federation", "licences_en_2011", "moins_de_20_ans", "entre_20_et_60_ans", "femmes",
                 "-------------------------------+-----------------------+--------------------"));
@@ -61,7 +64,16 @@ public class SimpleClient {
 
     public void queryValDeMarne() {
         // on execute une requête
-        ResultSet results = session.execute("SELECT * FROM sport.valdemarne ");
+        ResultSet results = session.execute("SELECT * FROM bddCassandra.valdemarne ");
+        // on affiche le résultat
+        System.out.println(String.format("%-10s\t%-10s\t%-20s\t%-70s\t%-30s\t%-30s\t%-30s\t%s-30s", "code_insee", "code_postal", "commune", "federation", "licences_en_2011", "moins_de_20_ans", "entre_20_et_60_ans", "femmes",
+                "-------------------------------+-----------------------+--------------------"));
+        displayResults(results);
+    }
+
+    public void querySeineSaintDenis() {
+        // on execute une requête
+        ResultSet results = session.execute("SELECT * FROM bddCassandra.seinesaintdenis ");
         // on affiche le résultat
         System.out.println(String.format("%-10s\t%-10s\t%-20s\t%-70s\t%-30s\t%-30s\t%-30s\t%s-30s", "code_insee", "code_postal", "commune", "federation", "licences_en_2011", "moins_de_20_ans", "entre_20_et_60_ans", "femmes",
                 "-------------------------------+-----------------------+--------------------"));
@@ -82,10 +94,14 @@ public class SimpleClient {
         // connection (localhost)
         client.connect("127.0.0.1");
         // création de la base de données (schema)
-        // client.createSchema();
-        // on interroge la base de données
-        client.queryEssone();
-        client.queryValDeMarne();
-        client.close();
+        try {
+            client.createSchema();
+        } catch (Exception e) {
+            // on interroge la base de données
+            client.queryEssone();
+            client.queryValDeMarne();
+            client.querySeineSaintDenis();
+            client.close();
+        }
     }
 }
